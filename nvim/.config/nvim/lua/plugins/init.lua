1e63d1238ceb421d94c8d979576606fa095e423d
Derek Tata <derekjtata@gmail.com>
2021-09-29 09:11:46 -0400

    Updated plugins
    Added config line to nvim-tree.lua
    Otherwise the plugin fails to load.

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -115,8 +115,9 @@ return packer.startup(function()
 
   -- Nvim Tree
   use {
-    "kyazdani42/nvim-tree.lua",
-    requires = "kyazdani42/nvim-web-devicons",
+    'kyazdani42/nvim-tree.lua',
+    requires = 'kyazdani42/nvim-web-devicons',
+    config = function() require'nvim-tree'.setup {} end
   }
 
   -- Plenary
